1edbd64f1a9ef7df40b7f3493929376e7e0533d6
JPhanGT3 <148407787+JPhanGT3@users.noreply.github.com>
2025-10-25 19:20:59 -0400

    Enhance test suite

--- TestTriangle.py
+++ TestTriangle.py
@@ -18,13 +18,27 @@ class TestTriangles(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
 
     def testRightTriangleA(self): 
-        self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
+        self.assertEqual(classifyTriangle(3,4,5),'Right')
 
-    def testRightTriangleB(self): 
-        self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
-        
     def testEquilateralTriangles(self): 
-        self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+        self.assertEqual(classifyTriangle(1,1,1),'Equilateral')
+
+    def testScaleneTriangles(self): 
+        self.assertEqual(classifyTriangle(7,8,9),'Scalene')
+        self.assertEqual(classifyTriangle(5,4,3),'Scalene')
+
+    def testIsocelesTriangles(self): 
+        self.assertEqual(classifyTriangle(5,5,6),'Isoceles')
+
+    def testInvalidInputs(self): 
+        self.assertEqual(classifyTriangle(201,201,201),'InvalidInput') # sides greater than 200
+        self.assertEqual(classifyTriangle(3,3,0),'InvalidInput') # side length of 0
+        self.assertEqual(classifyTriangle(3,4,False),'InvalidInput') # invalid type bool instead of int
+
+    def testNotTriangle(self): 
+        self.assertEqual(classifyTriangle(7,2,3),'NotATriangle') # a >= (b + c) 
+        self.assertEqual(classifyTriangle(2,6,3),'NotATriangle') # b >= (a + c)
+        self.assertEqual(classifyTriangle(2,4,8),'NotATriangle') # c >= (a + b)
 
 if __name__ == '__main__':
     print('Running unit tests')
